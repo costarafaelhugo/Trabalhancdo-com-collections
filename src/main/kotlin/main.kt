a627f55a7b1942e97c01bf6c02130c96f479888f
Hugo Costa <costarafaelhugo@gmail.com>
2021-04-08 10:11:52 -0300

    Testando comportamentos do Set

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,22 @@
 fun main() {
-    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos=assistiramCursoAndroid+assistiramCursoKotlin
-    println(assistiramAmbos.distinct())
+    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: MutableSet<String> = mutableSetOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos: MutableSet<String> = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
 
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList:MutableList<String> = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
 }
 
