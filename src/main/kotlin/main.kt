30e397be1f7423173ccb0f8e4c29b727390b84e3
Hugo Costa <costarafaelhugo@gmail.com>
2021-04-08 14:36:17 -0300

    Funções fix e comportamentos de escrita do MAP

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,35 @@
 fun main() {
-    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin: MutableSet<String> = mutableSetOf("Alex", "Paulo", "Maria")
-//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
-    val assistiramAmbos: MutableSet<String> = mutableSetOf<String>()
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
-    assistiramAmbos.add("Ana")
-    println(assistiramAmbos)
+ val pedidos: MutableMap<Int, Double> = mutableMapOf(
+     Pair(1, 20.0),
+     Pair(2, 34.0),
+     3 to 50.0
+ )
+    println(pedidos)
+   val pedido:Double? = pedidos[3]
+    pedido?.let {
+        println("pedido $it")
+    }
+    for (p: Map.Entry<Int,Double> in pedidos){
+        println("numedo do pedido: ${p.key}")
+        println("numedo do pedido: ${p.value}")
+    }
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5,80.0)
+    println(pedidos)
+    pedidos[1]=100.0
+    println(pedidos)
+    pedidos.putIfAbsent(6,200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6,300.0)
+    println(pedidos)
 
-    println(assistiramCursoAndroid + assistiramCursoKotlin)
-    println(assistiramCursoAndroid - assistiramCursoKotlin)
-    println(assistiramCursoKotlin - assistiramCursoAndroid)
-    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
-    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 50.0)
+    println(pedidos)
 
-    val assistiramList:MutableList<String> = assistiramAmbos.toMutableList()
-    assistiramList.add("Alex")
-    println(assistiramList)
-    println(assistiramList.toSet())
+
 }
+
 
