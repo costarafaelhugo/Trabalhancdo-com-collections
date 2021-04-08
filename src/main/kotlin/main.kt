1e9ad7655b839e1237765b3a95192e40319ef691
Hugo Costa <costarafaelhugo@gmail.com>
2021-04-08 08:46:14 -0300

    Copiando coleções

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,29 @@
 fun main() {
-    val nomes: Collection<String> = setOf(
+    val banco = BancoDeNomes()
+    val nomesSalvos:Collection<String> = banco.nomes
+//    banco.nomes.add("Alex")
+//    nomesSalvos.add("Tonho")
+    banco.salva("Hugo")
+    println(nomesSalvos)
+    println(banco.nomes)
+
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
         "Alex",
         "Fran",
         "Gui",
@@ -7,12 +31,11 @@ fun main() {
         "Ana"
     )
 
- for (nome in nomes){
-     println(nome)
- }
+    for (nome in nomes) {
+        println(nome)
+    }
 
     println(nomes)
     println("Tem o nome Alex ${nomes.contains("Alex")}")
     println("Tamanho do nome Alex é ${nomes.size}")
-
 }
