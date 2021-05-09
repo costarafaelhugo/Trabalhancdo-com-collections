923bf160c4b379cd5f03fe7cfe2034aefd428bf7
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-09 14:30:29 -0300

    Criei o Katas2 e adicionei o enunciado das funções

---
+++ src/main/kotlin/katas/katas2.kt
@@ -0,0 +1,38 @@
+package katas
+/*
+Katas
+Soma (1 ponto)
+Escreva uma função chamada "add" que pega dois argumentos e retorna a soma deles.
+Você pode usar operadores incorporados para finalizar a definição.
+Por exemplo, chamar add(2, 4) deve retornar um resultado 6.
+Multiplicação (2 pontos)
+Escreva uma função chamada "multiply" que pega dois argumentos e retorna seu produto.
+Você não pode usar operadores ou funções aritméticas incorporadas (como o operador de multiplicação *). Em vez disso, você precisará de um loop for que chama a função "add" criada anteriormente.
+Por exemplo, chamar multiply(6, 8) deve retornar um resultado 48.
+Potência (2 pontos)
+Escreva uma função chamada "power" que pega dois argumentos (x e n) e retorna o resultado de x elevado à potência n.
+Você não pode usar operadores ou funções aritméticas incorporadas (como o operador de multiplicação * ou o operador de potência/expoente **). Em vez disso, para escrever esta função, use outras funções que você escreveu em katas anteriores.
+Outra palavra usada para potência é "exponenciação". Por exemplo, se nós chamarmos power(2, 8), retornaremos 256 ao multiplicar 2 por ele mesmo 8 vezes.
+2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 = 256
+Se tivéssemos chamado power(3, 4), teríamos multiplicado 3 por ele mesmo 4 vezes:
+3 * 3 * 3 * 3 = 81
+Veja este artigo da Wikipédia para mais detalhes sobre exponenciação.
+Fatorial (2 pontos)
+Escreva uma função chamada "factorial" que pega um único argumento e retorna o fatorial dele.
+Você não pode usar operadores ou funções aritméticas incorporadas (como o operador de multiplicação *). Em vez disso, para escrever esta função, use outras funções que você escreveu em katas anteriores.
+Por exemplo, chamar factorial(4) deve retornar um resultado 24.
+Dicas
+A resposta para a maioria desses katas será similar. Ela envolverá tipicamente:
+a declaração de uma variável para manter o registro de um resultado final.
+a escrita de um loop for para modificar o resultado consistentemente.
+o retorno do resultado
+Bônus: Fibonacci (3 pontos)
+Escreva uma função chamada "fibonacci" que pega um argumento n e retorna o enésimo número Fibonacci.
+Você não pode usar operadores ou funções aritméticas incorporadas. Em vez disso, para escrever esta função, use outras funções que você escreveu em katas anteriores (como o operador de multiplicação incorporado *).
+Por exemplo, chamar fibonacci(8) deve retornar o resultado 13.
+0 1 1 2 3 5 8 [13] 21
+O número entre colchetes é o oitavo número fibonacci.
+ */
+fun main() {
+
+}
